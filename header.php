0af943afbe19b3a2a84ec91f8a356f990969a2a7
Louis Saffaro <louis.saffaro@gmail.com>
2021-07-21 16:13:06 +0000

    Upload New File

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <link rel="stylesheet" href="public/css/styles.css" type="text/css">
+		<title>
+			Bienvenu à l'Alpha-bêtise librairie !
+			
+		</title>
+		
+	</head>
+	<body>
+	 <section>
+		<header>
+		    <div><img id="calque_1" src="public/img/Logo/Logo-AlphaBetise-black-horizontal.svg" alt="Logo_Alphabêtise"></div>
+            <nav>
+				<ul>
+					<li><a href="index.php">Accueil</a></li>
+					<li><a href="rencontres.php">Rencontres</a></li>
+					<li><a href="la_librairie.php">La librairie</a></li>
+					<li><a href="contact.php">Contact</a></li>
+
+				</ul>
+            </nav>
+		</header>
+	</section>
+		
+	
