fe802da9e74211f956379ebbef7e5887274023ac
Louis Saffaro <louis.saffaro@gmail.com>
2021-07-21 16:07:40 +0000

    Upload New File

---
+++ rgpd.php
@@ -0,0 +1,20 @@
+
+
+<?php require_once "layouts/header.php"; ?>
+   
+   <section style="transform:translateX(0.2%);">
+   
+   <section id="sectionRGPD" style="width:100vw;height:20rem;background-color:blanchedalmond;">
+ <p  style="display:flex;justify-content:center;width:12rem;font-weight:bold;font-size:0.8rem;"> Aucune donnée n'est commercialisée ni revendue à un tiers.
+  Les données ne sont collectées que pour des finalités précises qui ont toutes pour objectif commun de vous fournir nos produits et nos services dans un souci constant d'améliorer notre offre et de vous satisfaire.
+    Nous ne collectons que les données qui sont nécessaires en fonction de leur finalité.
+    À tout moment vous pouvez vous inscrire ou vous désinscrire de nos newsletters…
+    Nous veillons à respecter vos droits et à répondre à toutes vos questions sur vos données.
+ </p>
+</section>
+
+</section>
+
+
+<?php require "layouts/footer.php" ?>
+
