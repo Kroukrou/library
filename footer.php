0e28ca6d7c09c727457fe2cf0802bd48eeead110
Louis Saffaro <louis.saffaro@gmail.com>
2021-07-21 16:14:06 +0000

    Upload New File

---
+++ footer.php
@@ -0,0 +1,20 @@
+<section>
+			<footer>
+				
+				<ul>
+            
+            		<li> &copy; 2021 </li>
+            		
+					<li>
+						<a href="rgpd.php">Conditions d'utilisation</a>
+					</li>
+            
+            	</ul>
+			
+			</footer>
+			<div id="Centre"><a href="index.php"><img id="calque_2" src="public/img/Logo/Logo-AlphaBetise-Color-Vertical.svg" alt="Logo_Alphabêtise"></a></div>
+            </section>
+		<script src="js/script.js"></script>
+		</body>
+</html>
+	
